7e68da13ee422574bad1770ce36cd6d8d2e952c1
kirillgolovko <kirillgolovko@yandex-team.ru>
2019-06-19 00:21:27 +0300

    Parsing in progress

--- src/main/java/ru/dev/kirillgolovko/grect/gcode/parcer/GcodeCoordinates.java
+++ src/main/java/ru/dev/kirillgolovko/grect/gcode/parcer/GcodeCoordinates.java
@@ -1,10 +1,27 @@
 package ru.dev.kirillgolovko.grect.gcode.parcer;
 
+import java.util.function.BiConsumer;
+
 public enum GcodeCoordinates {
-    X,
-    Y,
-    Z,
-    I,
-    J,
-    K;
+    X((val, builder) -> builder.addX(val)),
+    Y((val, builder) -> builder.addY(val)),
+    Z((val, builder) -> builder.addZ(val)),
+    I((val, builder) -> builder.addI(val)),
+    J((val, builder) -> builder.addJ(val)),
+    K((val, builder) -> builder.addK(val));
+
+    private BiConsumer<Double, VectorFromTokensBuilder> addToBuilder;
+
+    private GcodeCoordinates(BiConsumer<Double, VectorFromTokensBuilder> addToBuilder) {
+        this.addToBuilder = addToBuilder;
+    }
+
+    public static void parceNextCoordinateToken(Token token, VectorFromTokensBuilder builder) {
+        if (!token.tokenType.equals(TokenTypes.XYZ_COORDINATE) && !token.tokenType.equals(TokenTypes.IJK_COORDINATE)) {
+            throw new IllegalArgumentException("Token must be coordinate, got " + token.tokenType.toString());
+        }
+        String coordinate = token.originalString.substring(0, 1);
+        double value = Double.parseDouble(token.originalString.substring(1));
+        valueOf(coordinate).addToBuilder.accept(value, builder);
+    }
 }
--- src/main/java/ru/dev/kirillgolovko/grect/gcode/parcer/VectorFromTokensBuilder.java
+++ src/main/java/ru/dev/kirillgolovko/grect/gcode/parcer/VectorFromTokensBuilder.java
@@ -1,16 +1,61 @@
 package ru.dev.kirillgolovko.grect.gcode.parcer;
 
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.Map;
+
+import ru.dev.kirillgolovko.grect.math.Vector3D;
+
 public class VectorFromTokensBuilder {
 
-    private boolean XyzPresent;
+    private boolean XyzPresent = false;
     private double x = .0;
     private double y = .0;
     private double z = .0;
 
-    private boolean IjkPresent;
+    private boolean IjkPresent = false;
     private double i = .0;
     private double j = .0;
     private double k = .0;
 
+    public void addX(double x) {
+        this.x = x;
+        XyzPresent = true;
+    }
 
+    public void addY(double y) {
+        this.y = y;
+        XyzPresent = true;
+    }
+
+    public void addZ(double z) {
+        this.z = z;
+        XyzPresent = true;
+    }
+
+    public void addI(double i) {
+        this.i = i;
+        IjkPresent = true;
+    }
+
+    public void addJ(double j) {
+        this.j = j;
+        IjkPresent = true;
+    }
+
+    public void addK(double k) {
+        this.k = k;
+        IjkPresent = true;
+    }
+
+    public Map<GcofeVectorTypes, Vector3D> bake() {
+        Map<GcofeVectorTypes, Vector3D> result = new HashMap<>();
+        if(XyzPresent) {
+            result.put(GcofeVectorTypes.XYZ, new Vector3D(x, y, z));
+        }
+        if(IjkPresent) {
+            result.put(GcofeVectorTypes.IJK, new Vector3D(i, j, k));
+        }
+        return Collections.unmodifiableMap(result);
+    }
 }
