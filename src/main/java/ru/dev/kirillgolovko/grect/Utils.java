55227b2f0cb176861bc3110456423c69d5d10667
Lilia <iskhakovalilia@yandex.ru>
2019-06-29 14:02:52 +0300

    reading from File Matrix3D

--- src/main/java/ru/dev/kirillgolovko/grect/Utils.java
+++ src/main/java/ru/dev/kirillgolovko/grect/Utils.java
@@ -8,27 +8,29 @@ import java.nio.file.Files;
 import java.nio.file.Path;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
 import java.util.stream.Collectors;
 
 public class Utils {
-   public static  <U> List<U> repeat(U x, int times){
-       ArrayList<U> arrayList = new ArrayList<>();
-       for (int i = 0; i < times; ++i) {
-           arrayList.add(x);
-       }
-       return arrayList;
-   }
+    public static <U> List<U> repeat(U x, int times) {
+        ArrayList<U> arrayList = new ArrayList<>();
+        for (int i = 0; i < times; ++i) {
+            arrayList.add(x);
+        }
+        return arrayList;
+    }
 
-   public static double dot(List<Double> vector, double a) {
-       return vector.stream().collect(Collectors.summarizingDouble(v -> v * a)).getSum();
-   }
+    public static double dot(List<Double> vector, double a) {
+        return vector.stream().collect(Collectors.summarizingDouble(v -> v * a)).getSum();
+    }
 
-    public static Matrix3D readMatrixFromFile(String pathArg) throws IOException {
-        File file = new File(pathArg);
-        Path path = file.toPath();
-
-        List<String> rows = Files.readAllLines(path);
-
-        return Matrix3D.fromString(rows);
+    public static Optional<Matrix3D> readMatrixFromFile(String pathArg) {
+        Path path = new File(pathArg).toPath();
+        try {
+            List<String> rows = Files.readAllLines(path);
+            return Optional.of(Matrix3D.fromString(rows));
+        } catch (IOException ex) {
+            return Optional.empty();
+        }
     }
 }
